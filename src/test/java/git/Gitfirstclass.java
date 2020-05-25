59a031d1fc6cb1786a15f796d9f8f6e6bef1414a
vidhya12278 <evidhya60@gmail.com>
2020-05-25 11:05:12 +0530

    add two lines

--- src/test/java/git/Gitfirstclass.java
+++ src/test/java/git/Gitfirstclass.java
@@ -3,5 +3,7 @@ package git;
 public class Gitfirstclass {
 public static void main(String[] args) {
 	System.out.println("gitfirstclass");
+	System.out.println("cloning setup");
+	System.out.println("learning process");
 }
 }
